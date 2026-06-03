6c2f15a9fb96a791830da88340e015871346868e
Om Chouhan <om.chouhan@mylabconnect.com>
2026-06-04 03:17:48 +0530

    Added WhiteNoise

--- hrms_project/settings.py
+++ hrms_project/settings.py
@@ -46,14 +46,21 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'hrms_project.urls'
 
 TEMPLATES = [
@@ -119,15 +126,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Media Files
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-import os
+
+# Static Files
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(
     BASE_DIR,
     'staticfiles'
+)
+
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
 )
